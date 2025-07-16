5a57f4ba09d044152e46cd0a6d9f4ca88f905a07
leonl42 <noinnick8@gmail.com>
2025-07-17 00:23:12 +0200

    make runs

--- render.sh
+++ render.sh
@@ -1,3 +1,9 @@
-python render.py  -m output/compact3d/bicycle -s 360_v2/bicycle --load_quant 
-python render.py  -m output/orig/bicycle -s 360_v2/bicycle
-python render.py  -m output/mini/bicycle -s 360_v2/bicycle
+python render.py  -m output/compact3d/bonsai -s 360_v2/bonsai --load_quant 
+python render.py  -m output/mini_compact3d/bonsai -s 360_v2/bonsai --load_quant 
+python render.py  -m output/mini/bonsai -s 360_v2/bonsai
+python render.py  -m output/mini_lessdepth/bonsai -s 360_v2/bonsai
+python render.py  -m output/mini_lessdepth_opacity/bonsai -s 360_v2/bonsai
+python render.py  -m output/mini_opacity/bonsai -s 360_v2/bonsai
+python render.py  -m output/orig/bonsai -s 360_v2/bonsai
+python render.py  -m output/orig_depth_reg/bonsai -s 360_v2/bonsai
+python render.py  -m output/orig_opacity/bonsai -s 360_v2/bonsai
